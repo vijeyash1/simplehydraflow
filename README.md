ca76e1eb74270d7617494ef4a3fe6c3d11ac9707
vijeyash1 <vijeyash@gmail.com>
2023-01-10 23:25:06 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,100 @@
 # simplehydraflow
+## step 1
+
+***Install ory cli by following the steps given in the link below***
+[ory cli installation docs-- please click this link to install the ory cli](https://www.ory.sh/docs/guides/cli/installation)
+
+
+## step 2
+***once the ory cli is installed create a ory project by running the below line***
+*`ory create project`*
+
+## step 3
+***once the project is created run the below line to get the project ID***
+*`ory list projects`*
+
+## step 4
+***please note down the project ID you got from the above step***
+
+***now run the below line to create a oauth client in Hydra , use the Project ID that you got from the Previous step***
+
+    ory create oauth2-client --project <Your Project ID> \
+    
+    --name "nodeapp" \
+    
+    --grant-type authorization_code,refresh_token,client_credentials \
+    
+    --response-type code,id_token \
+    
+    --scope openid --scope offline_access --scope email \
+    
+    --redirect-uri http://127.0.0.1:5555/callback
+
+***The response will be similar to the one provided below, note down all those details***
+
+    CLIENT ID 505409xa-8ccd-4259-9444-c4f7281d8de7
+    
+    CLIENT SECRET cSYiFwbbOlqrgkjashsdf~ksGDt
+    
+    GRANT TYPES authorization_code, refresh_token, client_credentials
+    
+    RESPONSE TYPES code, id_token
+    
+    SCOPE openid offline_access email
+    
+    AUDIENCE
+    
+    REDIRECT URIS http://127.0.0.1:5555/callback
+
+## step 5
+
+***Execute the below line  to get the config file***
+
+    ory get oauth2-config <Your project ID> --format yaml > config.yaml
+## step 6 
+
+***modify the config as given below: ***
+
+    urls:
+    
+    consent: http://localhost:3000/consent
+    
+    error: /ui/error
+    
+    login: http://localhost:3000/login
+    
+    post_logout_redirect: /oauth2/fallbacks/logout
+    
+   ## step 7
+   ***after modifying the config files login and consent endpoints as given above execute the below line***
+
+    ory update oauth2-config <Your project ID> --file config.yaml
+
+## step 8
+
+***Run the go program which will serve the login and consent endpoints***
+
+    go run main.go
+
+## step 9
+
+***now execute the below lines , this will create a oauth client application that will run in http://localhost:5555***
+***This application will mimic the oauth client application in real world.***
+***Ory has created this awsome solution for the developers to test everything***
+
+***
+
+    ory perform authorization-code \
+      --client-id <Your client ID> \
+      --client-secret <Your client secret> \
+      --project <your Project ID> \
+      --port 5555 \
+      --scope openid
+
+***
+
+## step 10
+
+***now open http://localhost:5555***
+***voila you got the id token ***
+[https://jwt.io/](https://jwt.io/) use this link to decode the id token
---
+++ main.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/AlekSi/pointer"
+	"github.com/gin-gonic/gin"
+	ory "github.com/ory/client-go"
+)
+
+func main() {
+	r := gin.Default()
+	handler := NewHandler()
+	r.GET("/createclient", handler.CreateOauthClientHandler)
+	r.GET("/login", handler.AcceptLoginHandler)
+	r.GET("/consent", handler.AcceptConsentHandler)
+	r.Run(":3000")
+}
+
+type Handler struct {
+	ApiClient *ory.APIClient
+	ctx       context.Context
+	logger    *log.Logger
+}
+
+func NewHandler() *Handler {
+	conf := ory.NewConfiguration()
+	conf.Servers = ory.ServerConfigurations{{
+		URL: "https://unruffled-gauss-2yivnr1dcc.projects.oryapis.com",
+	}}
+	logger := log.New(os.Stderr, "app: ", log.LstdFlags|log.Lshortfile|log.Llongfile)
+	oryAuthedContext := context.WithValue(context.Background(), ory.ContextAccessToken, "ory_pat_ACe9AwgTyr9caySE3YTz1HoO08CyoVHW")
+	return &Handler{
+		ApiClient: ory.NewAPIClient(conf),
+		ctx:       oryAuthedContext,
+		logger:    logger,
+	}
+}
+
+// CreateOauthClientHandler creates a new Oauth2.0 client
+func (h *Handler) CreateOauthClientHandler(c *gin.Context) {
+	log.Println(h.ctx.Value(ory.ContextAccessToken))
+	_, res, err := h.ApiClient.OAuth2Api.CreateOAuth2Client(h.ctx).
+		OAuth2Client(ory.OAuth2Client{
+			ClientName:              pointer.ToString("auth-client-test"),
+			ClientSecret:            pointer.ToString("secret"),
+			GrantTypes:              []string{"authorization_code", "refresh_token"},
+			RedirectUris:            []string{"http://localhost:3000/callback"},
+			ResponseTypes:           []string{"code", "id_token"},
+			Scope:                   pointer.ToString("openid offline"),
+			TokenEndpointAuthMethod: pointer.ToString("client_secret_post"),
+		}).Execute()
+	defer res.Body.Close()
+	if err != nil {
+		h.logger.Println(res)
+		c.JSON(500, gin.H{
+			"response" : res,
+		})
+		return
+	}
+	c.JSON(200, gin.H{
+		"message": "Oauth2.0 client created successfully",
+	})
+}
+
+// func (h *Handler) GetOauthClientHandler(c *gin.Context) {
+// 	clientId := os.Getenv("CLIENT_ID")
+// 	client, _, err := h.ApiClient.OAuth2Api.GetOAuth2Client(h.ctx, clientId).Execute()
+// 	if err != nil {
+// 		h.logger.Println(err.Error())
+// 		c.JSON(500, gin.H{
+// 			"message": "Error getting Oauth2.0 client",
+// 		})
+// 		return
+// 	}
+// 	c.JSON(200, gin.H{
+// 		"message": client,
+// 	})
+// }
+
+// func (h *Handler) DeleteOauthClientHandler(c *gin.Context) {
+// 	clientId := os.Getenv("CLIENT_ID")
+// 	_, err := h.ApiClient.OAuth2Api.DeleteOAuth2Client(h.ctx, clientId).Execute()
+// 	if err != nil {
+// 		h.logger.Println(err.Error())
+// 		c.JSON(500, gin.H{
+// 			"message": "Error deleting Oauth2.0 client",
+// 		})
+// 		return
+// 	}
+// 	c.JSON(200, gin.H{
+// 		"message": "Oauth2.0 client deleted successfully",
+// 	})
+// }
+
+
+
+func (h *Handler) AcceptLoginHandler(c *gin.Context) {
+	challenge := c.Query("login_challenge")
+	accept, _, err := h.ApiClient.OAuth2Api.AcceptOAuth2LoginRequest(h.ctx).LoginChallenge(challenge).AcceptOAuth2LoginRequest(ory.AcceptOAuth2LoginRequest{
+		Remember:    pointer.ToBool(true),
+		RememberFor: pointer.ToInt64(3600),
+		Subject:     "vijey@gmail.com",
+	}).Execute()
+
+	if err != nil {
+		fmt.Println("error", err.Error())
+	}
+	c.Redirect(http.StatusFound, accept.RedirectTo)
+}
+
+func (h *Handler) AcceptConsentHandler(c *gin.Context) {
+	challenge := c.Query("consent_challenge")
+	fmt.Println("consent_challenge", challenge)
+	acceptConsentRes, _, err := h.ApiClient.OAuth2Api.AcceptOAuth2ConsentRequest(h.ctx).
+		ConsentChallenge(challenge).AcceptOAuth2ConsentRequest(
+		ory.AcceptOAuth2ConsentRequest{
+			GrantScope:  []string{"openid"},
+			Remember:    pointer.ToBool(true),
+			RememberFor: pointer.ToInt64(3600),
+			Session: &ory.AcceptOAuth2ConsentRequestSession{
+				IdToken: PersonSchemaJsonTraits{Email: "vijesh@gmail.com", Name: &PersonSchemaJsonTraitsName{First: pointer.ToString("vijesh"), Last: pointer.ToString("kumar")}},
+			},
+		}).Execute()
+	if err != nil {
+		fmt.Println("error", err.Error())
+	}
+	c.Redirect(http.StatusFound, acceptConsentRes.RedirectTo)
+}
+
+type PersonSchemaJsonTraits struct {
+	// Email corresponds to the JSON schema field "email".
+	Email string `json:"email" yaml:"email"`
+
+	// Name corresponds to the JSON schema field "name".
+	Name *PersonSchemaJsonTraitsName `json:"name,omitempty" yaml:"name,omitempty"`
+}
+
+type PersonSchemaJsonTraitsName struct {
+	// First corresponds to the JSON schema field "first".
+	First *string `json:"first,omitempty" yaml:"first,omitempty"`
+
+	// Last corresponds to the JSON schema field "last".
+	Last *string `json:"last,omitempty" yaml:"last,omitempty"`
+}
+
+// UnmarshalJSON implements json.Unmarshaler.
+func (j *PersonSchemaJsonTraits) UnmarshalJSON(b []byte) error {
+	var raw map[string]interface{}
+	if err := json.Unmarshal(b, &raw); err != nil {
+		return err
+	}
+	if v, ok := raw["email"]; !ok || v == nil {
+		return fmt.Errorf("field email in PersonSchemaJsonTraits: required")
+	}
+	type Plain PersonSchemaJsonTraits
+	var plain Plain
+	if err := json.Unmarshal(b, &plain); err != nil {
+		return err
+	}
+	*j = PersonSchemaJsonTraits(plain)
+	return nil
+}
+
+type PersonSchemaJson struct {
+	// Traits corresponds to the JSON schema field "traits".
+	Traits *PersonSchemaJsonTraits `json:"traits,omitempty" yaml:"traits,omitempty"`
+}
